a54458e11c597eeadafc047cecfed18845d89498
Aryan Pathare <33375954+AryanP281@users.noreply.github.com>
2020-03-11 19:38:59 +0530

    Add files via upload

--- multivariate_linear_regression.py
+++ multivariate_linear_regression.py
@@ -61,7 +61,7 @@ class Multivariate_Linear_Regression_Model(object) :
     def train_with_vectorized_gradient_descent(self, training_inputs, expected_outputs, learning_rate = 0.001, epochs = None, use_regularization=False, lmbda=0) :
         """Trains the model on the given data for the given number of epochs"""
 
-        X = self.generate_input_array(training_inputs) #Converting the training inputs to NumPy array
+        X = np.concatenate((np.ones((len(training_inputs), 1)), training_inputs), axis=1) #Converting the training inputs to NumPy array
 
         y = np.array(expected_outputs) #Coverting the expected outputs to NumPy array
         y.shape = (len(expected_outputs), 1) #Reshaping the expected outputs vector
@@ -71,22 +71,55 @@ class Multivariate_Linear_Regression_Model(object) :
             #Training for the given number of epochs
             for a in range(0, epochs) :
                 derivatives = np.zeros(self.parameters.shape)
-                Z = np.dot(X, self.parameters)
-                E = Z - y
 
-                #Calculating the cost function
-                self.cost_func_value = sum(E**2) / (2 * y.shape[0])
+                #Getting the outputs
+                forwardprop_res = self.forwardpropogate(X,y,self.parameters,lmbda) # (Z, E, J)
+                self.cost_func_value = forwardprop_res[2]
+                E = forwardprop_res[1]
 
-                #Calculating the gradients
-                derivatives = np.dot(X.T, E) / y.shape[0]
-
-                #Checking if regularization is to be used
-                if(use_regularization) :
-                    derivatives[1:-1] += self.parameters[1:-1] * (lmbda / y.shape[0])
-                    self.cost_func_value += (lmbda / (2 * y.shape[0])) * (self.parameters[1:-1] ** 2)
+                #Getting the gradients
+                derivatives = self.get_gradients(self.parameters, X, y, E, lmbda)
 
                 #Using gradient descent for optimizing the model parameters
                 self.gradient_descent(derivatives, learning_rate)
+
+    def forwardpropogate(self, X, y, parameters, lmbda) :
+        """Forward propogates and returns the outputs and cost function value for the given inputs.
+        X = inputs
+        y = expected outputs
+        parameters = parameters to be used
+        lmbda = Regularization Parameter
+        returns (Outputs, Errors, Cost)"""
+
+        #The model output using the given parameters
+        Z = np.dot(X, parameters)
+
+        #Calculating the cost function value
+        E = Z - y
+        j = sum(E**2) / (2 * y.shape[0])
+        j += (lmbda / (2 * y.shape[0])) * np.sum((parameters[1:-1] ** 2))
+
+        #Returning the outputs and cost
+        return (Z, E, j)
+
+    def get_gradients(self, params, X, y, E, lmbda) :
+        """Calculates and returns the gradients for the parameters
+        params = the parameters whose gradients are required,
+        X = inputs,
+        y = expected outputs,
+        E = errors in the model's outputs i.e (h - y),
+        lmbda = Regularization Parameter
+        return grads = a Numpy array containing the calculated gradients"""
+
+        grads = np.zeros(params.shape) #The gradients
+
+        #Calculating the gradients
+        grads = np.dot(X.T, E) / y.shape[0]  
+        grads += params[1:-1] * (lmbda / y.shape[0])
+
+        #Returning the calculated gradients
+        return grads
+
 
     def h(self, inputs) :
         """Calculates the output of the model i.e y = theta0*x_0 + theta1*x_1 + .... + theta_n*x_n = parameters.T * inputs"""
@@ -129,8 +162,8 @@ class Multivariate_Linear_Regression_Model(object) :
 
         self.cost_func_value = 0.0
 
-    def get_output(self, inputs) :
-        """Gets output from the model"""
+    def predict(self, inputs) :
+        """Gets prediction from the model"""
 
         #Converting the inputs to the required format
         x = np.array([self.x0] + inputs)
